aafd55eb161dfa67a023dff09c8863b0333b5bf0
petripykalainen <petri.pykalainen@bastu.net>
2019-10-22 19:04:39 +0300

    Initial commit and some boilerplate

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const Express = require('express');
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const app = Express();
+
+const sequelize = new Sequelize(
+process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,{
+  host: process.env.DB_HOST,
+  dialect: 'postgres'
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+// Environment
+const port = 3001 || process.env.port;
+app.use(Express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+  res.json({msg: "Hello world"});
+});
+
+app.listen(port, (req, res) => {
+  console.log(`Server running at port ${port}`);
+});
