ad6cdb957d91e7e34d0214c08659a2f4ef4859b5
petripykalainen <petri.pykalainen@bastu.net>
2019-10-26 19:45:06 +0300

    Connect to db, create get, post and delete routes

--- server/config/config.js
+++ server/config/config.js
@@ -1,35 +1,36 @@
- module.exports = {
-  "development": {
+require('dotenv').config();
+
+module.exports = { 
+   "development": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
-    "host": process.env.DB_HOSTNAME,
+    "host": process.env.DB_HOST,
     "dialect": "postgres",
+    "operatorsAliases": false 
+  },
+// "development": {
+//   "username": "pguser",
+//   "password": "secretpassword",
+//   "database": "secretdb",
+//   "host": "127.0.0.1",
+//   "dialect": "postgres",
+//   "operatorsAliases": false
+// }
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
     "operatorsAliases": false
   }
-  // "development": {
-  //   "username": "pguser",
-  //   "password": "secretpassword",
-  //   "database": "secretdb",
-  //   "host": "127.0.0.1",
-  //   "dialect": "postgres",
-  //   "operatorsAliases": false
-  // }
-// ,
-//   "test": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_test",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql",
-//     "operatorsAliases": false
-//   },
-//   "production": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_production",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql",
-//     "operatorsAliases": false
-//   }
 }
--- server/index.js
+++ server/index.js
@@ -1,33 +1,79 @@
 const Express = require('express');
-// const Sequelize = require('sequelize');
+const db = require('./models');
+const User = db.User;
 require('dotenv').config();
 
-const app = Express();
-
-// const sequelize = require();
-// new Sequelize(
-// process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,{
+// Option 1: Passing parameters separately
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
 //   host: process.env.DB_HOST,
 //   dialect: 'postgres'
 // });
 
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Connection has been established successfully.');
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
-//   });
+db.sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+const app = Express();
 
 // Environment
 const port = 3001 || process.env.port;
-app.use(Express.urlencoded({extended: false}));
+app.use(Express.urlencoded({extended: true}));
+app.use(Express.json());
+
+
 
 app.get('/', (req, res) => {
   res.json({msg: "Hello world"});
 });
 
-app.listen(port, (req, res) => {
+app.get('/users', (req, res) => {
+  User.findAll().then((users) => {
+    res.json({
+      users
+    });
+  });
+});
+
+app.post('/user', (req, res) => {
+  // console.log(req.body);
+  // res.json(req.body);
+  // User.create({firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email})
+  let {firstName, lastName, email} = req.body.user;
+  // User.findOne({where: { email }}).then((result) => {
+  //   console.log(result);
+  //   res.json({result})
+  // });
+  User.findOrCreate({where:{email}, defaults: { firstName, lastName }})
+    .then(([user, created]) => {
+      if (created) {
+        res.json({msg: 'Created new user', created});
+      } else {
+        res.json({msg: 'User already exists', user});
+      }
+    })
+});
+
+app.delete('/user', (req, res) => {
+  let {email} = req.body.user;
+  User.destroy({
+    where: {
+      email
+    }
+  }).then((result) => {
+    if (result) {
+      res.json({msg: "User deleted", result})
+    }else {
+      res.json({msg: "No such user!", result})      
+    }
+  });
+});
+
+app.listen(port, () => {
   console.log(`Server running at port ${port}`);
 });
+
